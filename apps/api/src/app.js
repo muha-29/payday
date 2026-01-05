fff5c73675d27ef960a8c582695458f25c89bc2a
shaikmuhammad183 <muhammadyounusshaik29@gmail.com>
2026-01-05 09:25:26 +0530

    Added alternative UI App

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(cors({
     origin: [
         'https://payday-xi.vercel.app',
+        'https://payday-879j.vercel.app',
         'http://localhost:3000'
     ],
     credentials: true
